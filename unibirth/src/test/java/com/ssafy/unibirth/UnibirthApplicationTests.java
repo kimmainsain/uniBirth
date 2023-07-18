81b0a11641c2aee5d38c2d7f75fe9ff45932cef5
codelee <leesy399@naver.com>
2023-07-18 15:51:13 +0900

    Initial commit

---
+++ unibirth/src/test/java/com/ssafy/unibirth/UnibirthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.unibirth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnibirthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
