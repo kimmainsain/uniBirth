67bbde2ac8009148c0844b38b4f6e3d1c748b085
devsain0621 <kmsid003@gmail.com>
2023-07-19 17:44:24 +0900

    keep

--- unibirth-fe/src/App.js
+++ unibirth-fe/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Main from "./components/메인/screens/Main";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div>이거 되나 안되나 추가를 해봅시다!</div>
       <div>준혁</div>
       <div>준혁jh</div>
+      <Main />
     </div>
   );
 }
