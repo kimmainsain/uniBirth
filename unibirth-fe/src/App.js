700165669cf9807185fe0af9c82807fb295affcc
junhyuk <jun509504@naver.com>
2023-07-20 17:53:05 +0900

    0720

--- unibirth-fe/src/App.js
+++ unibirth-fe/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/screens/Home";
 import Login from "./components/Home/screens/Login";
+import MyProfile from "./components/Profile/screens/MyProfile";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Login" element={<Login />} />
+      <Route path="/profiles" element={<MyProfile />} />
     </Routes>
   );
 }
--- unibirth-fe/src/components/Home/screens/Login.jsx
+++ unibirth-fe/src/components/Home/screens/Login.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-function Login() {
+const Login = () => {
   return (
     <div>
       <h1>로그인 페이지입니다.</h1>
     </div>
   );
-}
-
+};
 export default Login;
