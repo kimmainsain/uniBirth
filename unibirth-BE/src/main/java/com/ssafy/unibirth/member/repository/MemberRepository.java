2e51b910933045bdc831e98a62961cae2cdf0ef2
codelee <leesy399@naver.com>
2023-07-21 11:06:55 +0900

    chore: lee 멤버 테이블 생성

--- unibirth-BE/src/main/java/com/ssafy/unibirth/member/repository/MemberRepository.java
+++ unibirth-BE/src/main/java/com/ssafy/unibirth/member/repository/MemberRepository.java
@@ -3,4 +3,5 @@ package com.ssafy.unibirth.member.repository;
 import com.ssafy.unibirth.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
 }
