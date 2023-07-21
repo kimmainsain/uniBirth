0e19dbcbc22fc68bfe43829cc2785acc2a3f1aef
codelee <leesy399@naver.com>
2023-07-21 17:56:58 +0900

    chore: lee 엔티티 초안 작성

--- unibirth-BE/src/main/java/com/ssafy/unibirth/UnibirthApplication.java
+++ unibirth-BE/src/main/java/com/ssafy/unibirth/UnibirthApplication.java
@@ -2,8 +2,11 @@ package com.ssafy.unibirth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableJpaAuditing
 public class UnibirthApplication {
 
 	public static void main(String[] args) {
--- unibirth-BE/src/main/java/com/ssafy/unibirth/member/domain/Member.java
+++ unibirth-BE/src/main/java/com/ssafy/unibirth/member/domain/Member.java
@@ -1,23 +1,21 @@
 package com.ssafy.unibirth.member.domain;
 
+import com.ssafy.unibirth.common.whenAndWho.BaseTimeEntity;
 import com.ssafy.unibirth.zodiac.domain.Zodiac;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
 @Getter
 @NoArgsConstructor
-public class Member {
+public class Member extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
     private String name;
     private String nickname;
     private String password;
@@ -27,7 +25,7 @@ public class Member {
     private Role role; // USER, ADMIN, DELETED
 
     private int followingCount;
-    private int follwerCount;
+    private int followerCount;
     private int starCount; // 좋아요 수
 
     @ManyToOne
@@ -38,6 +36,14 @@ public class Member {
     private String introduction;
 
     private Date birth;
-    private String imageURL;
+    private String imageUrl;
+
+    // 멤버 상태를 삭제로 전환
+    // 멤버 자체를 데이터베이스에서 삭제해버리면 cascade된 별과 별자리까지 모두 삭제됨
+    // => 상태만 삭제된 것으로 바꿔주자
+    public void deleteMember() {
+        this.role = Role.DELETED;
+    }
+
 
 }
