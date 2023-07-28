6eaed7ce45722132fc2282a6d1b266a17b3cae03
KimRiun <kimriun27@gmail.com>
2023-07-28 13:26:25 +0900

    refactor: 기존 cors 삭제

--- unibirth-BE/src/main/java/com/ssafy/unibirth/follow/controller/FollowController.java
+++ unibirth-BE/src/main/java/com/ssafy/unibirth/follow/controller/FollowController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/profiles")
-//@CrossOrigin(origins = "*")
 public class FollowController {
 
     private final FollowService followService;
