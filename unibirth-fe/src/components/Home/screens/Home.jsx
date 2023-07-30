9c30542fa23dac3915d12d3b155cfbf1505acf72
seobstation <ljs8828@naver.com>
2023-07-30 16:30:59 +0900

    start_done

--- unibirth-fe/src/components/Home/screens/Home.jsx
+++ unibirth-fe/src/components/Home/screens/Home.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer1 from "../../../common/blocks/Footer1";
 import Button1 from "../../../common/atoms/Button1";
 import useMemberApi from "../../../api/useMemberApi";
 import useConstellationApi from "../../../api/useConstellationApi";
 import { useNavigation } from "../../../hooks/useNavigation";
 import Space from "../blocks/Space";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 
 const Home = () => {
   const { navigateToMainPlanet } = useNavigation();
@@ -67,13 +67,34 @@ const Home = () => {
     },
   ];
 
+  function SetCanvasSize() {
+    const { gl } = useThree();
+
+    useEffect(() => {
+      gl.setSize(1000, 800);
+    }, [gl]);
+
+    return null;
+  }
+
   return (
-    <div>
-      <h1>홈 화면입니다. </h1>
-      <Footer1 buttons={buttons} />
-      <Canvas>
-        <Space />
-      </Canvas>
+    <div className="relative h-screen w-screen">
+      <p className="absolute left-1/3 top-10 z-10 font-TAEBAEKmilkyway text-2xl text-white">
+        세상에서 가장 빛나는 너의 별
+      </p>
+      <p className="absolute left-1/3 top-20 z-10 font-TAEBAEKmilkyway text-4xl text-white">
+        uni-Birth
+      </p>
+      <div className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 transform">
+        <Footer1 buttons={buttons} />
+      </div>
+      <div className="absolute top-0 z-0 h-full w-full">
+        <Canvas camera={{ position: [0, -50, 0] }}>
+          <SetCanvasSize />
+          <color attach="background" args={["black"]} />
+          <Space />
+        </Canvas>
+      </div>
     </div>
   );
 };
