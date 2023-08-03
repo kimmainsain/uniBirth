54b0166b2d0d40b69e3245eeaab83992176b6fb9
junhyuk <jun509504@naver.com>
2023-08-03 16:28:42 +0900

    persist

--- unibirth-fe/src/recoil/atoms.jsx
+++ unibirth-fe/src/recoil/atoms.jsx
@@ -1,17 +1,24 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 export const emailState = atom({
   key: "emailState",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 export const passwordState = atom({
   key: "passwordState",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 export const nicknameState = atom({
   key: "nicknameState",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 export const targetNicknameState = atom({
   key: "targetNicknameState",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
