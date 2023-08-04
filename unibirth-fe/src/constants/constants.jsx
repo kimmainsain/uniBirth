5a7440b7e6752528b607b4ad21f97379573a7234
junhyuk <jun509504@naver.com>
2023-08-04 09:30:23 +0900

    0804morning

--- unibirth-fe/src/constants/constants.jsx
+++ unibirth-fe/src/constants/constants.jsx
@@ -1,5 +1,4 @@
 // API URL
-// export const API_URL = "http://3.35.135.57:8080";
-export const API_URL = "http://localhost:8080";
+export const API_URL = "http://3.35.135.57:8080";
 
 export const CONTENT_TYPE_JSON = "application/json";
