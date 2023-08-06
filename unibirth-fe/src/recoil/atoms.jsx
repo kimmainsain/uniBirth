65e95c2f8116f31fff93cc45dbde715ba845a69c
kimmainsain <kmsid003@gmail.com>
2023-08-06 16:29:19 +0900

    merge2

--- unibirth-fe/src/recoil/atoms.jsx
+++ unibirth-fe/src/recoil/atoms.jsx
@@ -13,7 +13,6 @@ export const targetNicknameState = atom({
   effects_UNSTABLE: [persistAtom],
 });
 
-
 export const starListState = atom({
   key: "starListState", // unique ID (with respect to other atoms/selectors)
   default: { starList: [] }, // default value (aka initial value)
