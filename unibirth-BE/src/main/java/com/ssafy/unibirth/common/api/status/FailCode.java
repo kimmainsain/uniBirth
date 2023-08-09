444f1997d54f9d0732cf563b54e3540b66bd80bf
KimRiun <kimriun27@gmail.com>
2023-08-09 15:20:09 +0900

    fix: 필터 관련 jwt 인증 오류 수정

--- unibirth-BE/src/main/java/com/ssafy/unibirth/common/api/status/FailCode.java
+++ unibirth-BE/src/main/java/com/ssafy/unibirth/common/api/status/FailCode.java
@@ -13,6 +13,7 @@ public enum FailCode {
 
     TOKEN_EXPIRED(FORBIDDEN, "만료된 토큰입니다."),
     INVALID_TOKEN(FORBIDDEN, "유효하지 않은 토큰입니다."),
+
     // 별자리
     PLANET_NOT_FOUND(NOT_FOUND, "존재하지 않은 행성입니다."),
     CONSTELLATION_NOT_FOUND(NOT_FOUND, "존재하지 않는 별자리입니다."),
