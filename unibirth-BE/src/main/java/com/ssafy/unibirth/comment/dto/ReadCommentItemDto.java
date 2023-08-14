ea13dad9ff897fbfc976bceb11ef747cb7e997ef
KimRiun <kimriun27@gmail.com>
2023-08-14 10:52:33 +0900

    feat: 별 조회시 댓글 리스트 조회 구현

---
+++ unibirth-BE/src/main/java/com/ssafy/unibirth/comment/dto/ReadCommentItemDto.java
@@ -0,0 +1,17 @@
+package com.ssafy.unibirth.comment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReadCommentItemDto {
+    private String nickname;
+    private String imageUrl;
+    private String content;
+    private LocalDateTime createdAt;
+}
