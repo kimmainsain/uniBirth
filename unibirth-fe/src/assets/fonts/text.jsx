865ecf2cec83fe31051f6e0206cd0f42b12127a1
kimmainsain <kmsid003@gmail.com>
2023-08-18 09:51:09 +0900

    unibirth-fe delete

--- unibirth-fe/src/assets/fonts/text.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export const Text = (text) => {
-  text = { text };
-  return <p className="text-italic text-lg">{text}</p>;
-};
--- unibirth-fe/src/constants/constants.jsx
+++
@@ -1,4 +0,0 @@
-// API URL
-export const API_URL = "http://3.35.135.57:8080";
-
-export const CONTENT_TYPE_JSON = "application/json";
