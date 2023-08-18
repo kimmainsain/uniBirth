8600ccff06de7d8a539a22d1ffb7ccde250fb8ac
kimmainsain <kmsid003@gmail.com>
2023-08-18 10:07:50 +0900

    END

---
+++ unibirth-fe/src/common/blocks/SearchHeader.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { useNavigation } from "../../hooks/useNavigation";
+import { SEARTCH_LIST } from "../../constants/constants";
+import Search from "../../assets/icons/js/search";
+import CustomDropdown from "../atoms/CustomDropdown";
+
+const SearchHeader = ({ buttons, category, setCategory, query, setQuery }) => {
+  const { navigateToSearchCommon } = useNavigation();
+
+  const handleSearchInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSearch = () => {
+    navigateToSearchCommon(query, category);
+  };
+
+  const handleCategoryChange = (newCategory) => {
+    setCategory(newCategory);
+    // 새 카테고리로 쿼리를 실행하는 로직을 여기에 추가
+  };
+
+  return (
+    <div className="flex justify-between space-x-4 px-2 py-4">
+      <div className="flex items-center">
+        {buttons &&
+          buttons.map((button, index) => {
+            const ButtonComponent = button.component;
+            return (
+              <ButtonComponent
+                key={index}
+                className="w-10 font-Pretendard"
+                value={button.value}
+                onClick={button.onClick}
+                icon={button.icon}
+              />
+            );
+          })}
+      </div>
+      <div className="flex flex-grow"></div>
+      <div className="flex items-center justify-end space-x-4 px-2">
+        <input
+          className="w-1/2 border-b bg-transparent font-Pretendard text-white"
+          type="text"
+          placeholder={query}
+          value={query}
+          onChange={handleSearchInputChange}
+        />
+        <div className="flex flex-col items-center justify-center rounded-lg border-double bg-transparent font-Pretendard">
+          <CustomDropdown
+            value={category}
+            onChange={handleCategoryChange}
+            searchList={SEARTCH_LIST}
+          />
+        </div>
+        <button className="font-Pretendard" onClick={handleSearch}>
+          <Search />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchHeader;
